b30e20c588bc7ef7b35e7945fc0dbc7b0113a014
yuygfgg <140488233+yuygfgg@users.noreply.github.com>
2025-03-11 20:50:41 +0800

    forget about load_and_process

--- vswobbly/__init__.py
+++ vswobbly/__init__.py
@@ -8,7 +8,7 @@ in VapourSynth scripts without needing to export .vpy files.
 __version__ = "0.2.0"
 
 # Export main functionality
-from .core import WobblySource, wobbly_source
+from .core import WobblySource, wobbly_source, load_and_process
 from .types import (
     FrameProperties, 
     FieldMatchOrder, 
@@ -22,6 +22,7 @@ from .errors import WobblyError, WobblyParseError, WobblyProcessError
 __all__ = [
     "WobblySource", 
     "wobbly_source",
+    "load_and_process",
     "FrameProperties",
     "FieldMatchOrder",
     "ProcessPosition",
